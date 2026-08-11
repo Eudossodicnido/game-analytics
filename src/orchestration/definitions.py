9346900b7ecad851f2288a83c90bc64a4824b61d
Luca Costantino <69009356+Eudossodicnido@users.noreply.github.com>
2026-08-11 12:58:38 +0200

    chore:linting

--- src/orchestration/definitions.py
+++ src/orchestration/definitions.py
@@ -1,4 +1,8 @@
 import dagster as dg
-from src.orchestration.defs.assets import raw_rawg_api, bronze_games, silver_and_gold_games
+from src.orchestration.defs.assets import (
+    raw_rawg_api,
+    bronze_games,
+    silver_and_gold_games,
+)
 
-defs = dg.Definitions(assets=[raw_rawg_api,bronze_games,silver_and_gold_games])
+defs = dg.Definitions(assets=[raw_rawg_api, bronze_games, silver_and_gold_games])
